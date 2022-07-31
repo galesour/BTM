8623430f405ee7c8d541ccb8f2a9854ada015a52
Niro Lin <linqinyun@outlook.com>
2022-07-31 17:44:25 -0400

    reformat and remove unused files

---
+++ BTM/src/infer.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+class Infer:
+    def __init__(self, infer_type, k):
+        self.type = infer_type
+        self.k = k
+        self.pz = None      # the probability proportion of K topics
+        self.pw_z = None
+
+    def run(self, docs_pt, model_dir):
+        self.load_para(model_dir)
+
+    def load_para(self, model_dir):
+        # load pz - the probability proportion of K topics
+        pt = open(model_dir + "k" + str(self.k) + ".pz")
+        if not pt:
+            Exception("Model file not found!")
+
+        for line in pt.readlines():
+            info = map(lambda x: float(x), line.strip().split())
+            self.pz = np.asarray(list(info))
+        assert (abs(self.pz.sum() - 1) < 1e-4)
+
+        # load pw_z - the probability proportion of each word in each topic
+        pt_2 = open(model_dir + "k" + str(self.k) + ".pw_z")
+        if not pt_2:
+            Exception("Model file not found!")
+
+        tmp = []
+        for line in pt_2.readlines():
+            info = map(lambda x: float(x), line.strip().split())
+            tmp.append(list(info))
+        self.pw_z = np.asarray(tmp)
+        print("n(z)=%d, n(w)=%d\n", self.pw_z.shape[0], self.pw_z.shape[1])
+        assert(self.pw_z.shape[0] > 0 and abs(self.pw_z[0].sum() - 1) < 1e-4)
--- BTM/src/str_util.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def vec2str(vec):
-    _str = ""
-    for v in vec:
-        _str += (v + ' ')
-    return _str
-
